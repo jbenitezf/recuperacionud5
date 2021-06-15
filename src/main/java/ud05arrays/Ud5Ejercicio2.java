c8905291ee473ba57ad61451e8bcbea8d8701a2a
jbenitezf <73657830+jbenitezf@users.noreply.github.com>
2021-06-15 12:42:19 +0200

    Initial commit

---
+++ src/main/java/ud05arrays/Ud5Ejercicio2.java
@@ -0,0 +1,38 @@
+package ud05arrays;
+
+public class Ud5Ejercicio2 {
+
+	//https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-expresiones-regulares-en-java/
+	//DNI con 10 digitos y 2Letras
+	//Fecha formato yyyy>mm>dd
+	
+	
+	/**
+	 * Devuelve una cadena con una expresión regular que sea capaz de
+	 * validad un nuevo tipo de DNI.
+	 * 
+	 * Tiene que cumplir, tener 10 dígitos 
+	 * seguido de dos LETRAS Mayúsculas entre A y Z
+	 * @return Expresión regular que valida los requisitos
+	 */
+	public static String validaDNI() {
+		return null;
+	}
+	
+	/**
+	 * Devuelve una cadena con una expresión regular que sea capaz de
+	 * de identificar si la cadena es una de las siguientes expresiones.
+	 * 
+	 * PERRO, GATO, TORTUGA, LORO
+	 * 
+	 * La expresión regular debe tener en cuenta dos casos para 
+	 * esos valores. Que la palabra esté entera en mayúsuculas.
+	 * o entera en minúsculas
+	 * 
+	 * @return Expresión regular que valida los requisitos
+	 */
+	public static String validaCampoEnumerado() {
+		return null;
+	}
+
+}
